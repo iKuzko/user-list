885988fa477f414a5d9b36734f13ad60ecd976d2
ikuzko <ikuzko@hotmail.com>
2026-02-09 18:44:27 +0200

    remove lazy loading for user routes to reduce js files number

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { UserProfile } from './users/components/user-info/user-profile';
+import {UserList} from './users/components/user-list/user-list';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full' },
-  { path: 'user', loadComponent: () => import("./users/components/user-list/user-list").then(c => c.UserList)},
+  { path: 'user', component: UserList},
   {
     path: 'user/:id',
-    loadComponent: () => import("./users/components/user-info/user-profile").then(c => c.UserProfile),
+    component: UserProfile,
     canDeactivate: [(component: UserProfile) => component.canDeactivate()],
   },
   { path: '**', redirectTo: 'user' },
